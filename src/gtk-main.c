799d70479c7755ba5237413c27271ae0145227ab
louib <L0U13@protonmail.com>
2020-04-16 22:20:01 -0400

    setup handy

--- src/gtk-main.c
+++ src/gtk-main.c
@@ -24,8 +24,8 @@
  */
 
 #include <gtk/gtk.h>
-// #define HANDY_USE_UNSTABLE_API
-// #include <handy.h>
+#define HANDY_USE_UNSTABLE_API
+#include <handy.h>
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
@@ -116,7 +116,7 @@ static void
 activate (GtkApplication* app,
           gpointer        user_data)
 {
-  GtkWidget *window;
+  GtkWidget* window;
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "macchanger");
@@ -128,10 +128,10 @@ int
 main (int    argc,
       char **argv)
 {
-  GtkApplication *app;
+  GtkApplication* app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new ("org.gnu.macchanger", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
