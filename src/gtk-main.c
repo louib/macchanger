14615d9f73e641b4a01fedc93e05a383e7f0d84a
louib <L0U13@protonmail.com>
2020-04-16 21:40:21 -0400

    Fixing build.

--- src/gtk-main.c
+++ src/gtk-main.c
@@ -119,7 +119,7 @@ activate (GtkApplication* app,
   GtkWidget *window;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_title (GTK_WINDOW (window), "macchanger");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
   gtk_widget_show_all (window);
 }
